5f4eb1f25a43a48876ac9ff4a1bdc9af4a997ede
unknown <lochaydeguerreiro@hotmail.com>
2025-05-19 03:53:27 -0300

    update 1.7

--- webhook-decoracao.php
+++ webhook-decoracao.php
@@ -1,4 +1,8 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 declare(strict_types=1);
 
 ini_set('display_errors', '1');
@@ -140,8 +144,6 @@ if ($enviarEmail) {
     require_once '/home1/paymen58/agencialed.com/email/PHPMailer/src/SMTP.php';
     require_once '/home1/paymen58/agencialed.com/email/PHPMailer/src/Exception.php';
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
 
     $mail = new PHPMailer(true);
     try {
