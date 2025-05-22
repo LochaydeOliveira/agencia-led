c17fad34d377dbb0d28199c74b95fd961104c3f4
unknown <lochaydeguerreiro@hotmail.com>
2025-05-22 03:07:21 -0300

    Atualiza Filtro v4

--- painel.php
+++ painel.php
@@ -135,9 +135,10 @@
         color: #0000008c;
         width: 15%;
         cursor: pointer;
+        transition: all 0.3s ease;
     }
 
-    .form-select {
+    .form-select:hover {
         color: #000000;
         font-weight: 600;
     }
