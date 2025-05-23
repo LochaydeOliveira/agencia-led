6e332789009a71f5946ed4ee54767509aeed4187
unknown <lochaydeguerreiro@hotmail.com>
2025-05-23 20:46:23 -0300

    Atualiza Acesso as Lista v26

--- painel.php
+++ painel.php
@@ -669,25 +669,27 @@ if ($cliente) {
                     <?php foreach ($todas_listas as $lista): ?>
                         <?php $liberado = in_array($lista['id'], $listas_com_acesso); ?>
                         <div class="col-md-6 col-lg-4 mb-4 fornecedor fade-in" data-category="<?php echo htmlspecialchars($lista['nome']); ?>" data-lista-id="<?php echo $lista['id']; ?>">
-                            <div class="card h-100 rounded-2 border-0">
+                            
+                            <?php if (!$liberado): ?>
+                                <div class="bloqueio-overlay">
+                                    <div class="style-bloqueio">
+                                        <img src="assets-agencia-led/icones-svg/trancar.svg" alt="trancar">
+                                        <strong>Lista bloqueada!</strong>
+                                    </div>
+                                    <div class="style-bloqueio-btn">
+                                        <p>Libere agora mesmo realizando o pagamento via Pix.</p>
+                                        <button class="btn btn-comprar-lista">Liberar Lista</button>
+                                    </div>                                      
+                                </div>
+                            <?php endif; ?>                    
+                        
+                        
+                            <div class="card h-100 rounded-2 border-0 <?php echo $liberado ? '' : 'blur'; ?>">
                                 <h5 class="card-title <?php echo $liberado ? '' : 'blur'; ?>"><?php echo htmlspecialchars($lista['nome']); ?></h5>
                                 <div class="card-body <?php echo $liberado ? '' : 'bloqueado'; ?>">
                                     <div class="conteudo-lista">
                                         <?php echo $liberado ? $lista['conteudo_html'] : ''; ?>
                                     </div>
-
-                                    <?php if (!$liberado): ?>
-                                        <div class="bloqueio-overlay">
-                                            <div class="style-bloqueio">
-                                                <img src="assets-agencia-led/icones-svg/trancar.svg" alt="trancar">
-                                                <strong>Lista bloqueada!</strong>
-                                            </div>
-                                            <div class="style-bloqueio-btn">
-                                                <p>Libere agora mesmo realizando o pagamento via Pix.</p>
-                                                <button class="btn btn-comprar-lista">Liberar Lista</button>
-                                            </div>                                      
-                                        </div>
-                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
