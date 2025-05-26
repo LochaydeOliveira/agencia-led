91e61aa042cf12a837d44cad7a1a96c2f57cf193
unknown <lochaydeguerreiro@hotmail.com>
2025-05-26 03:14:49 -0300

    Cria Pagina de Pedidos 12

---
+++ adm/sidebar.php
@@ -0,0 +1,12 @@
+<aside class="bg-light border-end p-3" style="width: 250px; min-height: 100vh;">
+  <h4 class="mb-4">Painel Admin</h4>
+  <nav class="nav flex-column">
+    <a href="index.php" class="nav-link mb-2">Dashboard</a>
+    <a href="clientes.php" class="nav-link mb-2">Clientes</a>
+    <a href="listas.php" class="nav-link mb-2">Listas</a>
+    <a href="pedidos.php" class="nav-link mb-2">Pedidos</a>
+    <a href="log.php" class="nav-link mb-2">Log</a>
+    <hr>
+    <a href="../logout.php" class="nav-link text-danger">Sair</a>
+  </nav>
+</aside>
