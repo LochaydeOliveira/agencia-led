1eeeb01528a8add078d0e1bdb9916e3eda50f20a
unknown <lochaydeguerreiro@hotmail.com>
2025-05-26 12:35:10 -0300

    Nova estrutura do painel admin - v1.1.4

--- adm/partials/footer.php
+++ adm/partials/footer.php
@@ -5,9 +5,6 @@
 <footer class="text-center text-muted py-2 small border-top bg-white">
   <ul class="list-inline mb-0">
     <li class="list-inline-item">
-      <a href="version.php">Versão <?php echo SISTEMA_VERSAO; ?></a>
-    </li>
-    <li class="list-inline-item">
       Desenvolvido por <a href="https://agencialed.com" target="_blank">Agência LED</a>
     </li>
   </ul>
--- version.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('SISTEMA_VERSAO', '1.0.0');
-define('ULTIMA_ATUALIZACAO', '2025-05-26');
