746f4ecbecc3cb271d16edfbc27bd51c755e7d61
unknown <lochaydeguerreiro@hotmail.com>
2025-05-28 22:45:43 -0300

    Atualiza Adm 8

--- adm/partials/sidebar.php
+++ adm/partials/sidebar.php
@@ -1,11 +1,20 @@
+<style>
+    .link-sidebar {
+    color: #7b8190;
+    font-weight: 600;
+    font-size: 13px;
+  }
+</style>
+
+
 <aside class="bg-light border-end p-3" style="width: 250px; min-height: 100vh;">
   <h4 class="mb-4">Painel Admin</h4>
   <nav class="nav flex-column">
-    <a href="index.php" class="nav-link mb-2">Dashboard</a>
-    <a href="clientes.php" class="nav-link mb-2">Clientes</a>
-    <a href="listas.php" class="nav-link mb-2">Listas</a>
-    <a href="pedidos.php" class="nav-link mb-2">Pedidos</a>
-    <a href="log.php" class="nav-link mb-2">Log</a>
+    <a href="index.php" class="nav-link mb-2 link-sidebar">Dashboard</a>
+    <a href="clientes.php" class="nav-link mb-2 link-sidebar">Clientes</a>
+    <a href="listas.php" class="nav-link mb-2 link-sidebar">Listas</a>
+    <a href="pedidos.php" class="nav-link mb-2 link-sidebar">Pedidos</a>
+    <a href="log.php" class="nav-link mb-2 link-sidebar">Log</a>
     <hr>
     <a href="partials/logout.php" class="nav-link text-danger">Sair</a>
   </nav>
