1bf4fee5231369ec1b75e8ff5ff68ada89015fd3
unknown <lochaydeguerreiro@hotmail.com>
2025-06-02 13:21:13 -0300

    Atualiza o Painel Cliente 15

--- painel.php
+++ painel.php
@@ -620,10 +620,11 @@
                 transition: background-color 0.3s ease, transform 0.3s ease;
                 font-size: 15px;
                 border-radius: 0!important;
-                color: #4364af!important;
+                color:rgb(0, 0, 0)!important;
                 text-align: left;
                 font-weight: 300;
                 margin: 0 0 1.5rem;
+                opacity: 0.7;
             }
 
             .conteudo-lista ul li a {
