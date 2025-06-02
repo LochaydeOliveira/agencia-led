23608fc63d3d79f804ae5fcc609ee8827e223ecb
unknown <lochaydeguerreiro@hotmail.com>
2025-06-02 13:34:35 -0300

    Atualiza o Painel Cliente 19

--- painel.php
+++ painel.php
@@ -747,10 +747,9 @@
                                                         <path d="M19,8.424V7A7,7,0,0,0,5,7V8.424A5,5,0,0,0,2,13v6a5.006,5.006,0,0,0,5,5H17a5.006,5.006,0,0,0,5-5V13A5,5,0,0,0,19,8.424ZM7,7A5,5,0,0,1,17,7V8H7ZM20,19a3,3,0,0,1-3,3H7a3,3,0,0,1-3-3V13a3,3,0,0,1,3-3H17a3,3,0,0,1,3,3Z"/>
                                                         <path d="M12,14a1,1,0,0,0-1,1v2a1,1,0,0,0,2,0V15A1,1,0,0,0,12,14Z"/>
                                                     </svg>
-                                                    <strong>Lista bloqueada!</strong>
                                                 </div>
                                                 <div class="style-bloqueio-btn">
-                                                    <p>Libere agora mesmo realizando o pagamento via Pix.</p>
+
                                                     <a href="<?php echo htmlspecialchars($lista['link_de_compra']); ?>"
                                                         target="_blank"
                                                         class="btn btn-comprar-lista">
