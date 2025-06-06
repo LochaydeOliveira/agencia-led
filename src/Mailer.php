a9c0800677ced2264ba8dabc750e7273afcae3fd
unknown <lochaydeguerreiro@hotmail.com>
2025-06-06 20:45:51 -0300

    Atualiza a Página de Clientes 31

--- src/Mailer.php
+++ src/Mailer.php
@@ -3,11 +3,11 @@
 define('BASE_PATH', dirname(__DIR__));
 
 // Carrega os arquivos principais do PHPMailer
-require_once BASE_PATH . '/email/PHPMailer/Exception.php'; // Classe de exceções
-require_once BASE_PATH . '/email/PHPMailer/SMTP.php';      // Classe para envio via SMTP
-require_once BASE_PATH . '/email/PHPMailer/PHPMailer.php'; // Classe principal
+require_once __DIR__ . '/../vendor/PHPMailer/Exception.php'; // Classe de exceções
+require_once __DIR__ . '/../vendor/PHPMailer/SMTP.php';      // Classe para envio via SMTP
+require_once __DIR__ . '/../vendor/PHPMailer/PHPMailer.php'; // Classe principal
 require_once __DIR__ . '/functions.php';                     // Funções utilitárias
-require_once BASE_PATH . '/config/email.php';               // Configurações do email
+require_once __DIR__ . '/../config/email.php';               // Configurações do email
 
 // Usa os namespaces corretos
 use PHPMailer\PHPMailer\PHPMailer;
