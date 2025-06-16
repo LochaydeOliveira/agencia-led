9dd18c2ad1b8d3e10909792b65222a09750957ca
unknown <lochaydeguerreiro@hotmail.com>
2025-06-16 00:23:40 -0300

    Ultimas Correções Gerais

--- atualizar_senha.sql
+++
@@ -1,4 +0,0 @@
--- Atualizar senha do usuário
-UPDATE usuarios 
-SET senha = '$2y$10$Mqq0ed.TjmCAmo61V39qCuy2p11QVZiOlUseehY5ogK3rHIbzNDEy'
-WHERE nome = 'lochayde'; 
--- criar_usuario.sql
+++
@@ -1,9 +0,0 @@
--- Criar novo usuário admin
-INSERT INTO usuarios (nome, email, senha, nivel, status) 
-VALUES (
-    'lochayde',
-    'lochaydeguerreiro@hotmail.com',
-    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-    'admin',
-    'ativo'
-); 
