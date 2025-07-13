845867866d4636ea1a0305ed4bbacd73ec4ecde9
unknown <lochaydeguerreiro@hotmail.com>
2025-07-13 14:30:19 -0300

    Atualização - Gera PDF 15

--- checklist-produto-vencedor/exportar_pdf.php
+++ checklist-produto-vencedor/exportar_pdf.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 if (!file_exists(__DIR__ . '/../../vendor/autoload.php')) {
     die('Autoload não encontrado em: ' . __DIR__ . '/../../vendor/autoload.php');
--- checklist-produto-vencedor/teste_autoload.php
+++ checklist-produto-vencedor/teste_autoload.php
@@ -10,7 +10,7 @@ echo 'Conteúdo do diretório vendor (na RAIZ):' . PHP_EOL;
 print_r(scandir(__DIR__ . '/../vendor'));
 echo '</pre>';
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 if (class_exists('Mpdf\\Mpdf')) {
     echo 'mPDF CARREGADO COM SUCESSO!';
