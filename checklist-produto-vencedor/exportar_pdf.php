0cffcf6c031141c03afb77bc315fa82e0897db71
unknown <lochaydeguerreiro@hotmail.com>
2025-07-13 15:30:05 -0300

    Atualização PDF

--- checklist-produto-vencedor/exportar_pdf.php
+++ checklist-produto-vencedor/exportar_pdf.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Receber dados do POST
 $pontos = $_POST['pontos'] ?? 0;
