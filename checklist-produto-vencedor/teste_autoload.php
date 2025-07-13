9a0654f1126fe1896682b6e4099324b863352fa1
unknown <lochaydeguerreiro@hotmail.com>
2025-07-13 13:54:32 -0300

    Atualização - Gera PDF 8

--- checklist-produto-vencedor/teste_autoload.php
+++ checklist-produto-vencedor/teste_autoload.php
@@ -2,14 +2,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$autoload = __DIR__ . '/../../vendor/autoload.php';
-if (!file_exists($autoload)) {
-    die('Autoload NÃO encontrado em: ' . $autoload);
-}
-require_once $autoload;
-
-if (class_exists('Mpdf\Mpdf')) {
-    echo 'mPDF CARREGADO COM SUCESSO!';
-} else {
-    echo 'mPDF NÃO encontrado!';
-} 
+echo '<pre>';
+echo 'PWD: ' . getcwd() . PHP_EOL;
+echo 'Conteúdo do diretório atual:' . PHP_EOL;
+print_r(scandir(__DIR__));
+echo 'Conteúdo do diretório vendor:' . PHP_EOL;
+print_r(scandir(__DIR__ . '/vendor'));
+echo '</pre>'; 
